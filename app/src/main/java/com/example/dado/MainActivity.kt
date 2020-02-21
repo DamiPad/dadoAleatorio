0907a32afb837f0a4a4c8a9d3f863f73a53ea573
Yaremith  Damian <laudamian27@gmail.com>
2020-02-21 09:06:55 -0600

    first comit

---
+++ app/src/main/java/com/example/dado/MainActivity.kt
@@ -0,0 +1,34 @@
+
+package com.example.dado
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+    public fun rollAleatorio(view: View) {
+        val random = Random.nextInt(6) + 1
+
+        val drawableResources = when (random) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        val dadoImage: ImageView = dado_image
+        dadoImage.setImageResource(drawableResources)
+    }
+}
